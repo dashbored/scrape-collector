141c19de9449407e2ca5add878fc148f9f8d02b6
Dashbored <richard_davy@hotmail.com>
2024-02-11 19:28:17 +0100

    Rewrote way status is logged and how scraper works

--- scrape-collector/Scraper.cs
+++ scrape-collector/Scraper.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -15,56 +16,67 @@ namespace scrape_collector
     {
         private static HttpClient _client = new();
         private Uri _baseAddress;
-        private int _maxUrls = 0;
-        private int _pageCount = 0;
+        private int _linkCount = 0;
+        private bool _done = false;
+        private readonly object _lock = new object();
+        private readonly char[] _spinner = new char[] { '-','\\','|','/'};
+        private int _spinnIndex = 0;
 
         private static HashSet<string> _visitedPaths = new HashSet<string>();
-        private ConcurrentQueue<Uri> _urls = new ConcurrentQueue<Uri>();
-        private ConcurrentDictionary<string, string> _pages = new ConcurrentDictionary<string, string>();
+        private ConcurrentDictionary<string, string> _links = new ConcurrentDictionary<string, string>();
         
         public Scraper(string url)
         {
             _baseAddress = new(url);
-            _urls.Enqueue(_baseAddress);
         }
 
         public async Task Scrape()
         {
 
-            Task.Run(async () => await StatusUpdate());
+            var statusUpdater = Task.Run(async () => await StatusUpdate());
             await ScrapeLinkAsync(_baseAddress);            
+            _done = true;
+            await statusUpdater;
         }
 
 
         private async Task StatusUpdate()
         {
-            var prevCounter = 0;
-            while (true)
+            var sw = new Stopwatch();
+            sw.Start();
+            var header = $"Scraping {_baseAddress}...";
+            while (!_done)
             {
-                if (_pageCount > prevCounter)
-                {
-                    Console.Clear();
-                    Console.WriteLine($"Pages:{_pageCount} ");
-                    prevCounter = _pageCount;
-                }
+                Console.Clear();
+                Console.WriteLine(header);
+                Console.WriteLine($"Links downloaded[{_spinner[_spinnIndex++%4]}]:{_linkCount} ");
 
-                await Task.Delay(10);
+                await Task.Delay(500);
             }
+
+            Console.Clear();
+            Console.WriteLine(header);
+            Console.WriteLine($"Done scraping. Scraped {_linkCount} links.");
+            Console.WriteLine($"Total time elapsed: {sw.Elapsed.ToString(@"hh\:mm\:ss\.fff")}");
+            sw.Stop();
+            await Task.CompletedTask;
         }
 
         public async Task ScrapeLinkAsync(Uri url)
         {
-            if (_pages.ContainsKey(url.LocalPath))
+            if (_links.ContainsKey(url.LocalPath))
             {
                 return;
             }
+
             var res = await _client.GetAsync(url);
             var html = await res.Content.ReadAsStringAsync();
-            if (!_pages.TryAdd(url.LocalPath, html))
+            if (!_links.TryAdd(url.LocalPath, html))
             {
                 return;
             }
-            Interlocked.Increment(ref _pageCount);
+
+            Interlocked.Increment(ref _linkCount);
             await SaveAsync(html, url);
             var anchors = HTMLParser.ParseHTMLforUris(html, url);
 
@@ -75,15 +87,15 @@ namespace scrape_collector
                 lock (_lock) 
                 { 
                     if (_visitedPaths.Contains(uri.LocalPath))
-                {
-                    continue;
-                }
+                    {
+                        continue;
+                    }
 
-                _visitedPaths.Add(uri.LocalPath);
+                    _visitedPaths.Add(uri.LocalPath);
                 }
                 tasks.Add(Task.Run(() => ScrapeLinkAsync(uri)));
             }
-            await Task.Delay(1);
+
             await Task.WhenAll(tasks);
         }
 
@@ -98,12 +110,15 @@ namespace scrape_collector
             {
                 name = name[1..];
             }
+
             var uri = new Uri(@$"C:\Temp\testfolder\");
             var p = Path.GetFullPath(Path.Combine(uri.LocalPath, name));
-            Directory.CreateDirectory(Path.GetDirectoryName(p));
+            if (!string.IsNullOrWhiteSpace(p))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(p)!);
             
-            await File.WriteAllTextAsync(p,response);
-            await Task.CompletedTask;
+                await File.WriteAllTextAsync(p,response);
+            }
         }
 
        
