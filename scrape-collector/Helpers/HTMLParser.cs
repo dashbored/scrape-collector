7789a28bd28cb41efdf0860f5d02a004760507f1
Dashbored <richard_davy@hotmail.com>
2024-02-11 17:48:35 +0100

    Updated to async methods

---
+++ scrape-collector/Helpers/HTMLParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace scrape_collector.Helpers
+{
+    public static class HTMLParser
+    {
+        public static HashSet<string> ParseHTMLforAnchors(string html)
+        {
+            var pattern = "<a\\s+(?:[^>]*?\\s+)?href=([\"'])(.*?)\\1";
+            var anchors = new HashSet<string>();
+            foreach (Match match in Regex.Matches(html, pattern, RegexOptions.IgnoreCase))
+            {
+                if (match.Success)
+                {
+                    anchors.Add(match.Groups[2].Value);
+                }
+            }
+            return anchors;
+        }
+
+        public static HashSet<Uri> ParseHTMLforUris(string html, Uri baseUri)
+        {
+            var pattern = "<a\\s+(?:[^>]*?\\s+)?href=([\"'])(.*?)\\1";
+            var anchors = new HashSet<Uri>();
+            foreach (Match match in Regex.Matches(html, pattern, RegexOptions.IgnoreCase))
+            {
+                if (match.Success)
+                {
+                    anchors.Add(new Uri(baseUri,match.Groups[2].Value));
+                }
+            }
+            return anchors;
+        }
+    }
+}
--- scrape-collector/Scraper.cs
+++ scrape-collector/Scraper.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using scrape_collector.Helpers;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -9,15 +11,17 @@ using System.Xml.Linq;
 
 namespace scrape_collector
 {
-    internal class Scraper
+    public class Scraper
     {
         private static HttpClient _client = new();
         private Uri _baseAddress;
-        private int _maxUrls = 0; 
+        private int _maxUrls = 0;
+        private int _pageCount = 0;
 
         private static HashSet<string> _visitedPaths = new HashSet<string>();
         private ConcurrentQueue<Uri> _urls = new ConcurrentQueue<Uri>();
         private ConcurrentDictionary<string, string> _pages = new ConcurrentDictionary<string, string>();
+        
         public Scraper(string url)
         {
             _baseAddress = new(url);
@@ -26,64 +30,80 @@ namespace scrape_collector
 
         public async Task Scrape()
         {
-            while (_urls.Count > 0)
+
+            Task.Run(async () => await StatusUpdate());
+            await GetLinkAsync(_baseAddress);
+        }
+
+
+        private async Task StatusUpdate()
+        {
+            var prevCounter = 0;
+            while (true)
             {
-                _maxUrls = Math.Max(_maxUrls, _urls.Count);
-                Console.Clear();
-                Console.WriteLine($"Urls:{_urls.Count} Pages:{_pages.Count} Max Urls: {_maxUrls}");
-                if (_urls.TryDequeue(out Uri url))
+                if (_pageCount > prevCounter)
                 {
-                    if (_pages.ContainsKey(url.LocalPath))
-                    {
-                        continue;
-                    }
-                    var res = await _client.GetAsync(url);
-                    var html = await res.Content.ReadAsStringAsync();
-                    if(!_pages.TryAdd(url.LocalPath, html))
-                    {
-                        continue;
-                    }
-
-                    await Save(html);
-                    var anchors = ParseHTMLforAnchors(html);
-
-                    foreach (var anchor in anchors)
-                    {
-                        var uri = new Uri(_baseAddress, anchor);
-                        if(_visitedPaths.Contains(uri.LocalPath))
-                        {
-                            continue;
-                        }
-                        _visitedPaths.Add(uri.LocalPath);
-
-                        if (_pages.ContainsKey(uri.LocalPath))
-                        {
-                            continue;
-                        }
-                         _urls.Enqueue(uri);
-                        
-                    }
+                    Console.Clear();
+                    Console.WriteLine($"Pages:{_pageCount} ");
+                    prevCounter = _pageCount;
                 }
+
+                await Task.Delay(10);
             }
         }
 
-        private async Task Save(string response)
+        public async Task GetLinkAsync(Uri url)
         {
+            if (_pages.ContainsKey(url.LocalPath))
+            {
+                return;
+            }
+            var res = await _client.GetAsync(url);
+            var html = await res.Content.ReadAsStringAsync();
+            if (!_pages.TryAdd(url.LocalPath, html))
+            {
+                return;
+            }
+            Interlocked.Increment(ref _pageCount);
+            await SaveAsync(html, url);
+            var anchors = HTMLParser.ParseHTMLforAnchors(html);
+
+            var tasks = new List<Task>();
+            foreach (var anchor in anchors)
+            {
+                var uris = new Uri(url, anchor);
+                var uri = new Uri(_baseAddress, uris);
+                if (_visitedPaths.Contains(uri.LocalPath) || _pages.ContainsKey(uri.LocalPath))
+                {
+                    continue;
+                }
+
+                _visitedPaths.Add(uri.LocalPath);
+                tasks.Add(Task.Run(() => GetLinkAsync(uri)));
+            }
+            await Task.Delay(1);
+            await Task.WhenAll(tasks);
+        }
+
+        private async Task SaveAsync(string response, Uri path)
+        {
+            var name = path.Segments.Length > 1 ? path.LocalPath : "index.html";
+            if (name[^1] == '\\' || name[^1] == '/')
+            {
+                name += "index.html";
+            }
+            if (name[0] == '\\' || name[0] == '/')
+            {
+                name = name[1..];
+            }
+            var uri = new Uri(@$"C:\Temp\testfolder\");
+            var p = Path.GetFullPath(Path.Combine(uri.LocalPath, name));
+            Directory.CreateDirectory(Path.GetDirectoryName(p));
+            
+            await File.WriteAllTextAsync(p,response);
             await Task.CompletedTask;
         }
 
-        public HashSet<string> ParseHTMLforAnchors(string html)
-        {
-            var pattern = "<a\\s+(?:[^>]*?\\s+)?href=([\"'])(.*?)\\1";
-            var anchors = new HashSet<string>();
-            foreach (Match match in Regex.Matches(html, pattern, RegexOptions.IgnoreCase))
-            {
-                if (match.Success)
-                {
-                    anchors.Add(match.Groups[2].Value);
-                }
-            }
-            return anchors;
-        }
+       
     }
 }
