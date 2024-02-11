4ccb43ae0e532ad359ee2a9020cd3e96342f517f
Dashbored <richard_davy@hotmail.com>
2024-02-11 19:27:16 +0100

    Changed project type to DLL and added separate Console application

---
+++ scrape-collector.TUI/Program.cs
@@ -0,0 +1,4 @@
+﻿using scrape_collector;
+var scraper = new Scraper("http://books.toscrape.com");
+await scraper.Scrape();
+Console.WriteLine();
--- scrape-collector/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using scrape_collector;
-using System.Runtime.CompilerServices;
-
-var scraper = new Scraper("http://books.toscrape.com");
-await scraper.Scrape();
-Console.ReadLine();
-
-
